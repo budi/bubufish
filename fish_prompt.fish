496636c084c3c1a74302740a6c6d716eec09ed3b
Budi Dharmawan <29maret@gmail.com>
2016-12-09 12:58:49 +0700

    December Updates

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,5 +1,3 @@
-set -g pad " "
-
 ## Function to show a segment
 function prompt_segment -d "Function to show a segment"
   # Get colors
@@ -19,39 +17,33 @@ end
 ## Function to show current status
 function show_status -d "Function to show the current status"
   if [ $RETVAL -ne 0 ]
-    prompt_segment normal red "✖ "
-    set pad ""
-  end
-  if [ -n "$SSH_CLIENT" ]
-    prompt_segment normal blue "SSH: "
-    set pad ""
-  end
-end
-
-function show_virtualenv -d "Show active python virtual environments"
-  if set -q VIRTUAL_ENV
-    set -l venvname (basename "$VIRTUAL_ENV")
-    prompt_segment normal white " ($venvname)"
+    prompt_segment FDAE66 D52116 " ✖ "
+    if [ -n "$SSH_CLIENT" ]
+      prompt_segment 8FE9FC FDAE66 ""
+      prompt_segment 8FE9FC 444759 " SSH: "
+      prompt_segment AF8EF9 8FE9FC ""
+    else
+      prompt_segment AF8EF9 FDAE66 ""
+    end
   end
 end
 
 # Show directory
 function show_pwd -d "Show the current directory"
   set -l pwd (prompt_pwd)
-  prompt_segment normal blue "$pad$pwd "
+  prompt_segment AF8EF9 282A36 " $pwd "
+  prompt_segment 5F63AB AF8EF9 ""
+  prompt_segment normal 5F63AB ""
 end
 
 # Show prompt w/ privilege cue
 function show_prompt -d "Shows prompt with cue for current priv"
-  set -l uid (id -u $USER)
-  if [ $uid -eq 0 ]
-    prompt_segment normal red " ✖ "
-    set_color normal
-    echo -n -s " "
-  else
-    prompt_segment normal white "→ "
+  if test -e .ruby-version
+    set -l ruby_version (rbenv version | awk '{print $1}')
+    prompt_segment normal red " ∴ $ruby_version"
+    prompt_segment normal white " →"
   end
-
+  prompt_segment normal white " "
   set_color normal
 end
 
@@ -59,7 +51,6 @@ end
 function fish_prompt
   set -g RETVAL $status
   show_status
-  show_virtualenv
   show_pwd
   show_prompt
   eval $HOME/.bubu/tmux-gitbar/update-gitbar
