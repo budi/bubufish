2485a3e515687c66f68f7e3b87bcac026b0072ff
Budi Dharmawan <29maret@gmail.com>
2017-03-02 03:43:16 +0530

    virtualenv

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,20 +1,15 @@
-## Function to show a segment
+## SHOW A SEGMENT
 function prompt_segment -d "Function to show a segment"
-  # Get colors
   set -l bg $argv[1]
   set -l fg $argv[2]
-
-  # Set 'em
   set_color -b $bg
   set_color $fg
-
-  # Print text
   if [ -n "$argv[3]" ]
     echo -n -s $argv[3]
   end
 end
 
-## Function to show current status
+## SHOW CURRENT STATUS
 function show_status -d "Function to show the current status"
   if [ $RETVAL -ne 0 ]
     prompt_segment FDAE66 D52116 " ✖ "
@@ -22,13 +17,25 @@ function show_status -d "Function to show the current status"
       prompt_segment 8FE9FC FDAE66 ""
       prompt_segment 8FE9FC 444759 " SSH: "
       prompt_segment AF8EF9 8FE9FC ""
+    else if set -q VIRTUAL_ENV
+      prompt_segment 61E47D FDAE66 ""
     else
       prompt_segment AF8EF9 FDAE66 ""
     end
   end
 end
 
-# Show directory
+## SHOW VIRTUALENV
+function show_virtualenv -d "Show active python virtual environments"
+  set -gx VIRTUAL_ENV_DISABLE_PROMPT "true"
+  if set -q VIRTUAL_ENV
+    set -l venvname (basename "$VIRTUAL_ENV")
+    prompt_segment 61E47D 282A36 " $venvname "
+    prompt_segment AF8EF9 61E47D ""
+  end
+end
+
+## SHOW DIRECTORY
 function show_pwd -d "Show the current directory"
   set -l pwd (prompt_pwd)
   prompt_segment AF8EF9 282A36 " $pwd "
@@ -36,16 +43,7 @@ function show_pwd -d "Show the current directory"
   prompt_segment normal 5F63AB ""
 end
 
-# Show virtualenv
-# function show_virtualenv -d "Show active python virtual environments"
-#   if set -q VIRTUAL_ENV
-#     set -l venvname (basename "$VIRTUAL_ENV")
-#     prompt_segment AF8EF9 282A36 " $pwd "
-#     prompt_segment normal green "($venvname) "
-#   end
-# end
-
-# Show prompt w/ privilege cue
+## SHOW PROMPT W/ PRIVILEGE CUE
 function show_prompt -d "Shows prompt with cue for current priv"
   if test -e .ruby-version
     set -l ruby_version (rbenv version | awk '{print $1}')
@@ -56,12 +54,12 @@ function show_prompt -d "Shows prompt with cue for current priv"
   set_color normal
 end
 
-## SHOW PROMPT
+## SHOW PROMPT RESULT
 function fish_prompt
   set -g RETVAL $status
   show_status
+  show_virtualenv
   show_pwd
-  # show_virtualenv
   show_prompt
   if [ "$TMUX" != "" ]
     eval $HOME/.bubu/tmux-gitbar/update-gitbar
